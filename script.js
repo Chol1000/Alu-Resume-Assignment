9c5a502b74498604a0e220f8add8214987d80fb5
Chol1000 <c.monykuch@alustudent.com>
2024-10-14 19:38:18 -0700

    Project completed

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // script.js
+// Add an event listener to the menu toggle button to show/hide navigation links
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('active'); // Toggle the active class to show/hide links
@@ -7,7 +8,7 @@ document.querySelector('.menu-toggle').addEventListener('click', function() {
 
 // Wait for the DOM to fully load
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll('.about-section, .skills-section, .projects-section, .contact-section'); // Select your sections
+    const sections = document.querySelectorAll('.about-section, .skills-section, .education-section, .experience-section, .projects-section, .contact-section'); // Select your sections
 
     const options = {
         threshold: 0.1 // Trigger when 10% of the section is visible
